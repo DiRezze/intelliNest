1543607850420e0f6bfa63df256759a80f5f9c20
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-08-29 07:36:25 -0300

    create: src/components

---
+++ src/components/testCard.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet ,View, Text } from "react-native";
+
+const TestCard = () => {
+    return(
+        <View style={styles.container}>
+            <Text>Este é um card de test</Text>
+        </View>
+    )
+}
+
+export default TestCard;
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+
+});
