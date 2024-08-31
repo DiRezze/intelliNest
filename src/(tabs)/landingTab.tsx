2a25c90fb63d10fa1cd9006ef94c660840298ad3
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-08-31 14:03:03 -0300

    landingTab styles

--- src/(tabs)/landingTab.tsx
+++ src/(tabs)/landingTab.tsx
@@ -1,19 +1,25 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
 
 const LandingTab = () => {
   return (
-    <View style={styles.tabContainer}>
-      <Text style={styles.title}>IntelliNest</Text>
-      <View style={styles.buttonsRow}>
-        <TouchableOpacity style={styles.signinButton}>
-            <Text style={styles.signinText}>Cadastro</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.loginButton}>
-            <Text style={styles.loginText}>Login</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    <LinearGradient
+        style={styles.tabContainer}
+        colors={['#4EB7AC','#A9E5CF']}
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+    >
+        <Text style={styles.title}>IntelliNest</Text>
+        <View style={styles.buttonsRow}>
+            <TouchableOpacity style={styles.signinButton}>
+                <Text style={styles.signinText}>Cadastro</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.loginButton}>
+                <Text style={styles.loginText}>Login</Text>
+            </TouchableOpacity>
+        </View>
+    </LinearGradient>
   );
 }
 
@@ -22,39 +28,46 @@ export default LandingTab
 const styles = StyleSheet.create({
     tabContainer: {
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
     },
     buttonsRow: {
         display: 'flex',
+        marginTop: 'auto',
         flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
         flexWrap: 'nowrap',
-        width: '100%'
+        width: '100%',
+        bottom: 0
     },
     title: {
         color: '#FFFFFF',
-        fontSize: 30,
+        marginTop: '60%',
+        fontSize: 48,
         fontWeight: 'bold',
-        marginBottom: 'auto',
         textAlign: 'center',
     },
     signinButton: {
-
+        flex: 1,
+        height: 58,
+        justifyContent: 'center',
     },
     signinText: {
-        fontSize: 48,
-        fontWeight: 'semibold',
-        textAlign: 'center',
-        color: '#4EB7AC'
-    },
-    loginButton:{
-
-    },
-    loginText: {
-        fontSize: 48,
+        fontSize: 32,
         fontWeight: 'semibold',
         textAlign: 'center',
         color: '#FFFFFF'
+    },
+    loginButton:{
+        flex: 1,
+        height: 58,
+        borderTopLeftRadius: 32,
+        justifyContent: 'center',
+        backgroundColor: 'white',
+    },
+    loginText: {
+        fontSize: 32,
+        fontWeight: 'semibold',
+        textAlign: 'center',
+        color: '#4EB7AC'
     },
 });
--- src/routes/tabs.routes.tsx
+++ src/routes/tabs.routes.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import { Feather } from '@expo/vector-icons';
-import Home from "../(tabs)/home";
+import Home from "../(tabs)/homeTab";
+import LandingTab from '../(tabs)/landingTab';
 
 const Tab = createBottomTabNavigator();
 
@@ -19,6 +20,14 @@ export default function TabRoutes(){
                 options={{
                     tabBarIcon: ({color, size}) => <Feather name="home" color={color} size={size} />,
                     tabBarLabel: "Home",
+                }}
+            />
+            <Tab.Screen
+                name="landing"
+                component={LandingTab}
+                options={{
+                    tabBarIcon: ({color, size}) => <Feather name="code" color={color} size={size} />,
+                    tabBarLabel: "Landing"
                 }}
             />
         </Tab.Navigator>
