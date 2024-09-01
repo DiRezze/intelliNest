ffdc91d1a4e28587a6735c27337c91a49ae3f829
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-09-01 11:30:23 -0300

    create: login & signin tabs

---
+++ src/(tabs)/loginTab.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const loginTab = () => {
+  return (
+    <View>
+      <Text>loginTab</Text>
+    </View>
+  )
+}
+
+export default loginTab
+
+const styles = StyleSheet.create({})
