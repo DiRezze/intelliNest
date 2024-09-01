7545311ab537175fff836a7410d642e26f2f2929
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-09-01 14:43:08 -0300

    add: app base colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,3 +1,14 @@
 export const colors = {
+    //main UI
     ui_gray: '#CBCACA',
+    ui_white: '#FEFEFE',
+
+    //titles and general text
+    colorTitle: '#5DBE78',
+    hyperLinks: '#82CB96',
+    inputFieldText: '#868686',
+
+    //buttons and interactable components
+    inputField: '#F8F8F8',
+    mainButton: '#5DBE78',
 }
