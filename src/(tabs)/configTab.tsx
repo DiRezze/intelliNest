317a5563adf6d52db7939ad64b26b7a915a74319
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-09-09 20:28:22 -0300

    create: main app tabs

---
+++ src/(tabs)/configTab.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const configTab = () => {
+  return (
+    <View>
+      <Text>configTab</Text>
+    </View>
+  )
+}
+
+export default configTab
+
+const styles = StyleSheet.create({})
---
+++ src/(tabs)/hardwareTab.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const hardwareTab = () => {
+  return (
+    <View>
+      <Text>hardwareTab</Text>
+    </View>
+  )
+}
+
+export default hardwareTab
+
+const styles = StyleSheet.create({})
