acaf7e5efa956524b224dd0f3b1379368e00093e
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-09-10 09:46:36 -0300

    userAuth UX features

--- src/(tabs)/debugTab.tsx
+++ src/(tabs)/debugTab.tsx
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { doSignOut } from '../config/auth'
+import { colors } from '../constants/colors'
 
 const DebugTab = () => {
   return (
-    <View>
+    <View style={styles.container}> 
       <Text>DebugTab</Text>
+      <TouchableOpacity 
+        style={styles.logout}
+        onPress={()=>doSignOut()}
+      >
+        <Text style={styles.btnText}>LogOut</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default DebugTab
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  logout: {
+    padding: 12,
+    borderRadius: 4,
+    width: 'auto',
+    backgroundColor: 'grey'
+  },
+  btnText : {
+    color: 'white'
+  },
+})
