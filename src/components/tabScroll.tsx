bfda3fbf1337fc52fe770c1d384a279f76ad0c5c
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-09-13 10:59:57 -0300

    fix: tabScroll

--- src/components/tabScroll.tsx
+++ src/components/tabScroll.tsx
@@ -1,12 +1,11 @@
 import { ScrollView, StyleSheet } from 'react-native'
 import React, { ReactNode } from 'react'
-import BlankCard from './blankCard';
 
 interface TabScroll { children: ReactNode; }
 
-const TabScroll: React.FC<BlankCard> = ({children}) => {
+const TabScroll: React.FC<TabScroll> = ({children}) => {
   return (
-    <ScrollView contentContainerStyle={styles.tabContainer}>
+    <ScrollView contentContainerStyle={styles.tabContainer} showsVerticalScrollIndicator={false}>
         {children}
     </ScrollView>
   )
@@ -17,7 +16,7 @@ export default TabScroll
 const styles = StyleSheet.create({
     tabContainer: {
         marginTop: 16,
-        flex: 1,
+        flexGrow: 1,
         alignItems: 'center'
     },
 });
